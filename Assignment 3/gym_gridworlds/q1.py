8d342f0f91db4f4f4fdff003c7512eb048eff7fa
Dikshuy <dikshant1212hsr@gmail.com>
2024-09-12 15:45:21 -0600

    value iteration done

--- Assignment 3/gym_gridworlds/q1.py
+++ Assignment 3/gym_gridworlds/q1.py
@@ -6,8 +6,8 @@ import seaborn as sns
 
 def bellman_updates(V, pi, R, P, T, gamma):
     v = V.copy()                                    # copying initial policy
-    P_V = np.multiply(np.dot(P, V), (1-T))         # pi*V and removing the terminal state
-    V = np.sum(pi * (R + gamma * P_V),axis=1)      # updating state value function
+    P_V = np.multiply(np.dot(P, V), (1-T))          # pi*V and removing the terminal state
+    V = np.sum(pi * (R + gamma * P_V), axis=1)      # updating state value function
 
     bellman_error = np.sum(np.abs(V - v))
 
@@ -58,25 +58,60 @@ def optimality_update(s, V, R, P, T, gamma):
     return V_s
 
 
-def value_iteration(V, R, P, T, gamma, theta, history):
+def value_iteration_update(V, R, P, T, gamma, theta, history):
     while True:
         delta = 0
         v = V.copy()
-        for s in range(n_states):
-            V_s = optimality_update(s, V, R, P, T, gamma)
-            V[s] = np.max(V_s)
-            delta = max(delta, np.abs(v[s]- V[s]))
-        history.append(delta)
+        Q = R + gamma * np.multiply(np.dot(P, V), (1 - T))
+        V = np.max(Q, axis=1)
+        delta = max(delta, np.max(np.abs(V - v)))
+        history.append(np.max(np.abs(V - v)))
         if delta < theta:
-                break
-        
+            break
+
     pi = np.ones((n_states, n_actions)) / n_actions
 
     for s in range(n_states):
         V_s = optimality_update(s, V, R, P, T, gamma)
         best = np.argmax(V_s)
         pi[s] = np.eye(n_actions)[best]
-    
+
+    return V, pi, history
+
+
+def value_iteration(V, R, P, T, gamma, theta, history):
+    while True:
+        v = V
+        V, pi, history = value_iteration_update(V, R, P, T, gamma, theta, history)
+
+        if np.max(np.abs(V-v)) < theta:
+            break
+
+    return V, pi, history
+
+
+def generalized_policy_iteration(V, R, P, T, gamma, theta, history, eval_steps=5):
+    pi = np.ones((n_states, n_actions)) / n_actions
+    while True:
+        for _ in range(eval_steps):
+            V, _ = policy_evaluation(V, pi, R, P, T, gamma, theta, history)
+            # history.append(bellman_error)
+
+        new_pi, _ = policy_improvement(V, pi, R, P, T, gamma)
+
+        if np.all(pi == new_pi):
+            # print(pi)
+            # print("------")
+
+            # print(new_pi)
+            # print("------")
+            # print(optimal_policy())
+            # print("-----**********************-")
+
+            break
+
+        pi = new_pi
+
     return V, pi, history
 
 
@@ -151,51 +186,69 @@ if __name__ == "__main__":
         # plot for V-function and convergence history
         fig1, axs1 = plt.subplots(2, len(gammas), figsize=(15, 10))
         fig2, axs2 = plt.subplots(2, len(gammas), figsize=(15, 10))
+        fig3, axs3 = plt.subplots(2, len(gammas), figsize=(15, 10))
         fig1.suptitle(f"State-Value Function $V_0$: {init_value}")
         fig2.suptitle(f"State-Value Function $V_0$: {init_value}")
+        fig3.suptitle(f"State-Value Function $V_0$: {init_value}")
 
         for i, gamma in enumerate(gammas):
-            V = np.full(n_states, init_value)
+            V_PI_init = np.full(n_states, init_value)
             history_PI = []
-            V, pi_learnt_PI, history_PI = policy_iteration(V, R, P, T, gamma, theta, history_PI)
-            
+            V_PI, pi_learnt_PI, history_PI = policy_iteration(V_PI_init, R, P, T, gamma, theta, history_PI)
+
             if np.all(pi_learnt_PI == pi_opt):
                 print("optimal policy found using policy iteration")
 
-            V = np.full(n_states, init_value)
+            V_VI_init = np.full(n_states, init_value)
             history_VI = []
-            V, pi_learnt_VI, history_VI = value_iteration(V, R, P, T, gamma, theta, history_VI)
-            print(V)
-            print()
-            print(history_VI)
-            print("------------")
-            
+            V_VI, pi_learnt_VI, history_VI = value_iteration(V_VI_init, R, P, T, gamma, theta, history_VI)
+
             if np.all(pi_learnt_VI == pi_opt):
                 print("optimal policy found using value iteration")
+
+            V_GPI_init = np.full(n_states, init_value)
+            history_GPI = []
+            V_GPI, pi_learnt_GPI, history_GPI = generalized_policy_iteration(V_GPI_init, R, P, T, gamma, theta, history_GPI)
+
+            print(V_GPI)
+            # print("--")
+            # print(pi_learnt_GPI)
+            # print("-------------------")
+
+            if np.all(pi_learnt_GPI == pi_opt):
+                print("optimal policy found using generalized policy iteration")
 
             grid_size = int(np.sqrt(n_states))
 
             axs1 = np.expand_dims(axs1, axis=1)
             axs2 = np.expand_dims(axs2, axis=1)
+            axs3 = np.expand_dims(axs3, axis=1)
 
             # plot V function
-            plot_v_function(V, axs1[0][i], gamma, grid_size)
-            plot_v_function(V, axs2[0][i], gamma, grid_size)
+            plot_v_function(V_PI, axs1[0][i], gamma, grid_size)
+            plot_v_function(V_VI, axs2[0][i], gamma, grid_size)
+            plot_v_function(V_GPI, axs3[0][i], gamma, grid_size)
 
             # plot convergence history
             axs1[1][i].plot(history_PI)
-            axs1[1][i].set_title(f'Convergence History, $\gamma$ = {gamma}')
+            axs1[1][i].set_title(f'Convergence history of Policy Iteration , $\gamma$ = {gamma}')
             axs1[1][i].set_xlabel('Iteration')
             axs1[1][i].set_ylabel('Bellman Error')
 
             # plot convergence history
             axs2[1][i].plot(history_VI)
-            axs2[1][i].set_title(f'Convergence History, $\gamma$ = {gamma}')
+            axs2[1][i].set_title(f'Convergence history of Value Iteration, $\gamma$ = {gamma}')
             axs2[1][i].set_xlabel('Iteration')
             axs2[1][i].set_ylabel('Bellman Error')
 
+            # plot convergence history
+            axs3[1][i].plot(history_GPI)
+            axs3[1][i].set_title(f'Convergence history of Generalized Policy Iteration, $\gamma$ = {gamma}')
+            axs3[1][i].set_xlabel('Iteration')
+            axs3[1][i].set_ylabel('Bellman Error')
+
         plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-        # plt.show()
+        plt.show()
         # filename_v = f'plots_v0_{init_value}.png'
         # plt.savefig(filename_v)
         # plt.close(fig1)
